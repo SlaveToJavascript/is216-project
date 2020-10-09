0900a3e0e49f0b3831ff998184b99e46fff0f5b2
Cindy <zxx.njc@gmail.com>
2020-10-10 01:56:25 +0800

    Home page (modules accordion, quotes)

---
+++ src/components/LeftNav.vue
@@ -0,0 +1,37 @@
+<template>
+    <sidebar-menu @toggle-collapse.stop :menu="menu" :width="width" :hideToggle="hideToggle">
+      <span slot="toggle-icon">Menu</span>
+    </sidebar-menu>
+</template>
+
+<script>
+    export default {
+        name: "LeftNav",
+        data() {
+            return {
+                menu: [
+                    {
+                        header: true,
+                        title: "Navigation",
+                    },
+                    {
+                        href: "/",
+                        title: "Home",
+                        icon: {
+                            element: "span",
+                            class: "glyphicon glyphicon-home"
+                        }
+                    }
+                ],
+                width: "200px",
+                hideToggle: true,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .v-sidebar-menu .vsm--toggle-btn {
+        display: none;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,88 +1,43 @@
 <template>
   <div class="home">
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-    />
     <!-- TOP NAV BAR -->
-    <div class="top-navbar">
-      <b-navbar toggleable="sm" type="light" variant="light">
-        <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
-
-        <b-navbar-brand class="navbar-header">Welcome to STUD</b-navbar-brand>
-
-        <b-collapse id="nav-text-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-text>Navbar text</b-nav-text>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </div>
+    <TopNav />
 
     <!-- LEFT NAVIGATION MENU -->
-    <sidebar-menu :menu="menu" :width="width">
-      <span slot="toggle-icon">Menu</span>
-    </sidebar-menu>
+    <LeftNav/>
+
+    <!-- QUOTES -->
+    <QuoteBar />
+
+    <!-- MODULES -->
+    <ModulesAccordian mod1="IDP" mod2="BPAS" mod3="WAD2" mod4="Ethics" mod5="MC" />
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import TopNav from '@/components/TopNav'
+import QuoteBar from '@/components/QuoteBar'
+import LeftNav from '@/components/LeftNav'
+import ModulesAccordian from '@/components/ModulesAccordian'
 
 export default {
   name: "Home",
-  components: {},
+  components: {
+    TopNav, 
+    QuoteBar,
+    LeftNav,
+    ModulesAccordian,
+  },
   data() {
     return {
-      menu: [
-        {
-          header: true,
-          title: "Navigation",
-          hiddenOnCollapse: true
-        },
-        {
-          href: "/",
-          title: "Home",
-          icon: {
-            element: "span",
-            class: "glyphicon glyphicon-home"
-          }
-        }
-      ],
-      width: "200px",
-      navbarOptions: {
-        elementId: "main-navbar",
-        isUsingVueRouter: true,
-        mobileBreakpoint: 992,
-        brandImagePath: "./",
-        // brandImage: require("../assets/images/logo.jpg"),
-        // brandImageAltText: "brand-image",
-        // showBrandImageInMobilePopup: true,
-        ariaLabelMainNav: "Main Navigation",
-        tooltipAnimationType: "shift-away",
-        menuOptionsRight: [
-          {
-            type: "button",
-            text: "Signup",
-            path: { name: "signup" },
-            class: "button-red"
-          },
-          {
-            type: "button",
-            text: "Login",
-            path: { name: "login" },
-            iconRight:
-              '<svg id="i-arrow-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"> <path d="M22 6 L30 16 22 26 M30 16 L2 16" /> </svg>'
-          }
-        ]
-      }
+      
     };
   }
 };
 </script>
 
 <style>
-  .navbar-header {
-    margin-left: 200px;
-  }
+  
 </style>
